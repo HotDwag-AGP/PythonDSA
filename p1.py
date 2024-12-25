27b56469a4c50093a61ee0397abe1a9d2896664c
Aagaman <aagamanpokharel45@gmail.com>
2024-12-26 00:17:34 +0545

    problem 1

--- p1.py
+++ p1.py
@@ -1,1 +1,7 @@
-//test 
+# syntax of a function is shown below:
+def NAME_Function(parameter):
+    #arguement
+    pass
+
+# Now using this framework build a layer of functions to
+# perform basic calculations!
