b72d56fabd1548d1c19ddc05b0d6f1f9c31f0003
Aagaman <aagamanpokharel45@gmail.com>
2024-12-26 16:19:11 +0545

    Rewrite this to include your solution

--- p1.py
+++ p1.py
@@ -1,7 +1,1 @@
-# syntax of a function is shown below:
-def NAME_Function(parameter):
-    #arguement
-    pass
-
-# Now using this framework build a layer of functions to
-# perform basic calculations!
+#SOLUTION TO P1 
